f64b7ffae0b529f102e7bb6659906dec686dcb6f
Khushant Wankhede <yashwankhede53@gmail.com>
2025-02-18 16:53:38 +0530

    Update Negative_Indentity.py

--- Negative_Indentity.py
+++ Negative_Indentity.py
@@ -3,12 +3,12 @@ import cv2
 # cv2.imshow('My image',img)
 i=1
 while i<11:
-    img = cv2.imread(f'{i}.jpg')
-    cv2.imshow(f'My image{i}',img)
-    negative = 255-img 
-    cv2.imshow(f'Negative{i}',negative)
+    img = cv2.imread(f'{i}.jpg')          #read image
+    cv2.imshow(f'My image{i}',img)        #show original image
+    negative = 255-img                     #conter to negative
+    cv2.imshow(f'Negative{i}',negative)   #show negative image
     i=i+1
-cv2.waitKey(5500)
+cv2.waitKey(5000)        #to wait image for 5 second 
 cv2.destroyAllWindows()
 # negative2 = 255-img2
 # cv2.imshow('NegativeTwo',negative2)
